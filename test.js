854fb897437ae432bfba7aeac040b36ac08f41e7
Tim Caswell <tim@creationix.com>
2014-10-21 18:02:50 -0500

    Start feeling out duktape

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-print("Hello ducky world.");
+try {
+
+  var timer = uv.new_timer()
+  uv.timer_start(timer, 300, 300, function () {
+    print("TIMEOUT!", timer);
+    assert(this === timer);
+    uv.timer_stop(timer);
+    uv.close(timer, function () {
+      print("CLOSE!", timer);
+      assert(this === timer);
+    }
+  });
+
+  uv.run();
+
+}
+catch (err) {
+  print(err.stack);
+}
