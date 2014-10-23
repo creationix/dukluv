389262268aaa2490ad0e354ea016fb6e7c5453f8
Tim Caswell <tim@creationix.com>
2014-10-23 00:11:26 -0500

    Expose global as global and uv_setup_args

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "duv.h"
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[]) {
   duk_context *ctx = NULL;
   uv_loop_t loop;
   uv_loop_init(&loop);
+
+  uv_setup_args(argc, argv);
 
   if (argc < 2) {
     fprintf(stderr, "Usage: dukluv script.js\n");
@@ -19,6 +21,8 @@ int main(int argc, const char *argv[]) {
   loop.data = ctx;
 
   duk_push_global_object(ctx);
+  duk_dup(ctx, -1);
+  duk_put_prop_string(ctx, -2, "global");
   dukopen_duv(ctx);
   duk_put_prop_string(ctx, -2, "uv");
 
